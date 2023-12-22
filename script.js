86994785c4f3eba0a4b651609b60e8d0fdee10dd
Delgermaa1230 <145009550+Delgermaa1230@users.noreply.github.com>
2023-12-22 22:17:21 +0800

    audios of grid container items

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-function createStars() {
+function showInfoAndPlaySound(planetName) {
+    // Play the sound associated with the clicked planet
+    playPlanetSound(planetName);
+  }
+  
+  function playPlanetSound(planetName) {
+    // Create an audio element
+    const audio = new Audio();
+  
+    // Set the source of the audio file based on the clicked planet
+    audio.src = `./sounds/${planetName.toLowerCase()}.mp3`;
+  
+    // Play the audio
+    audio.play();
+  }
+  
+  function createStars() {
     const container = document.querySelector("body");
     for (let i = 0; i < 1000; i++) {
-      // Increase the number of stars to 1000
       const star = document.createElement("div");
       star.className = "star";
       star.style.width = ".1px";
@@ -12,4 +27,5 @@ function createStars() {
     }
   }
   createStars();
+  
   
